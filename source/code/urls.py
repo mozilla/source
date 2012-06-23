1c38b0fced08acdc5cb2d625d25047b6615fc863
Ryan Pitts <ryan.a.pitts@gmail.com>
2012-06-22 22:25:09 -0700

    hook up tag urls and filtering for code objects

--- source/code/urls.py
+++ source/code/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls.defaults import *
+from django.views.generic.simple import redirect_to
+
 from .views import CodeList, CodeDetail
 
 
@@ -8,6 +10,18 @@ urlpatterns = patterns('',
         view = CodeList.as_view(),
         kwargs = {},
         name = 'code_list',
+    ),
+    url(
+        regex = '^tags/(?P<tag_slug>[-\w]+)/$',
+        view = CodeList.as_view(),
+        kwargs = {},
+        name = 'code_list_by_tag',
+    ),
+    url(
+        regex = '^tags/$',
+        view = redirect_to,
+        kwargs  = {'url': '/code/'},
+        name = 'code_list_tags',
     ),
     url(
         regex = '^(?P<slug>[-\w]+)/$',
--- source/code/views.py
+++ source/code/views.py
@@ -1,14 +1,24 @@
+from django.shortcuts import get_object_or_404
 from django.views.generic import ListView, DetailView
 
 from .models import Code
+from taggit.models import Tag
 
 
 class CodeList(ListView):
     model = Code
 
+    def get_queryset(self):
+        queryset = Code.objects.filter(is_live=True)
+        if 'tag_slug' in self.kwargs:
+            queryset = queryset.filter(tags__slug=self.kwargs['tag_slug'])
+        return queryset
+
     def get_context_data(self, **kwargs):
         context = super(CodeList, self).get_context_data(**kwargs)
         context['active_nav'] = 'Code'
+        if 'tag_slug' in self.kwargs:
+            context['tag'] = get_object_or_404(Tag, slug=self.kwargs['tag_slug'])
         return context
 
 
