3948d20dcb1056ed1b13dc625427ae73a85db281
Ryan Pitts <ryan.a.pitts@gmail.com>
2012-09-28 18:44:19 -0700

    filter js commenting, add error message if no matches

--- media/js/listfilter.js
+++ media/js/listfilter.js
@@ -1,22 +1,38 @@
+// icontains for text matching
 $.expr[':'].icontains = function(a, i, m) {
     return (a.textContent || a.innerText || "").toUpperCase().indexOf(m[3].toUpperCase()) >= 0;
 };
+
 $(document).ready(function() {
-    $('.js-filter-form').show();
+    // only provide filter form if js is enabled
+    $('#js-filter-form').show();
     var listFilter = $('#list-filter');
     var filteredList = $('#filterable-list');
     $(listFilter).change(function() {
         var filterVal = $(this).val();
         if (filterVal) {
+            // hide items that don't have matching text, lists that don't have
+            // visible items, and blocks that don't have visible lists
             $(filteredList).find('li:not(:icontains(' + filterVal + '))').hide();
+            $(filteredList).find('.filter-list:not(:has(li:visible))').hide();
             $(filteredList).find('.filter-block:not(:has(li:visible))').hide();
-            $(filteredList).find('.link-list:not(:has(li:visible))').hide();
-            $(filteredList).find('.link-list:has(li:icontains(' + filterVal + '))').show();
+            // show blocks/lists/items that contain matching text
             $(filteredList).find('.filter-block:has(li:icontains(' + filterVal + '))').show();
+            $(filteredList).find('.filter-list:has(li:icontains(' + filterVal + '))').show();
             $(filteredList).find('li:icontains(' + filterVal + ')').show();
         } else {
-            $(filteredList).find("li").show();
+            // nothing in filter form, so make sure everything is visible
             $(filteredList).find(".filter-block").show();
+            $(filteredList).find(".filter-list").show();
+            $(filteredList).find("li").show();
+        }
+        
+        // show 'no results' message if we've removed all the items
+        if ($('.filter-list > li:visible').length == 0) {
+            $('#no-results').remove();
+            $('#js-filter-form').after('<p id="no-results">No matching results found.</p>');
+        } else {
+            $('#no-results').remove();
         }
         return false;
     }).keyup(function() {
