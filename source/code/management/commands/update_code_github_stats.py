b257c812d9f58faddfced46305a732b7b6c8e09c
Ryan Pitts <ryan.a.pitts@gmail.com>
2013-06-17 15:07:07 -0700

    move github stats update into class method

--- source/code/management/commands/update_code_github_stats.py
+++ source/code/management/commands/update_code_github_stats.py
@@ -1,18 +1,18 @@
 '''
-Uses the GitHub API to update stats for Organization records.
+Uses the GitHub API to update stats for Code repos.
 '''
 from datetime import datetime
-import dateutil.parser
+#import dateutil.parser
 import logging
-import requests
+#import requests
 
 from django.conf import settings
 from django.core.management.base import BaseCommand
 
 from source.code.models import Code
 
-CLIENT_ID=settings.GITHUB_CLIENT_ID
-CLIENT_SECRET=settings.GITHUB_CLIENT_SECRET
+#CLIENT_ID=settings.GITHUB_CLIENT_ID
+#CLIENT_SECRET=settings.GITHUB_CLIENT_SECRET
 
 logging.basicConfig(filename='github_code_update.log', filemode='w', level=logging.INFO)
 
@@ -20,35 +20,45 @@ class Command(BaseCommand):
     help = 'Uses GitHub API to update stats for Code records.'
     def handle(self, *args, **options):
         logging.info('Started update: %s' % datetime.now())
-        # get all the Person records with Twitter usernames
+        # get all the Code records with that have GitHub repos
         code_list = Code.objects.filter(url__icontains='//github.com/')
         
         for code in code_list:
-            github_location = code.url.split('github.com/')[1]
-            github_user, github_repo = github_location.split('/')
-            github_api_url = 'https://api.github.com/repos/%s/%s?client_id=%s&client_secret=%s' % (
-                github_user.lower(), github_repo.lower(),
-                CLIENT_ID, CLIENT_SECRET
-            )
+            # attempt to fetch stats from GitHub API
+            updated = code.update_github_stats()
 
-            r = requests.get(github_api_url)
-            data = r.json
-
-            try:
-                # handle GitHub API's ISO8601 timestamps
-                last_push = data['pushed_at'].strip('Z')
-                last_push = dateutil.parser.parse(last_push, fuzzy=True)
-                code.repo_last_push = last_push
-                # the rest of the API data
-                code.repo_forks = data['forks']
-                code.repo_watchers = data['watchers']
-                code.repo_description = data['description']
-                code.repo_master_branch = data['master_branch']
+            # if we get stats, save them to the database. Otherwise
+            # log the error.
+            if updated:
                 code.save()
                 logging.info('Succesful update: %s' % github_location)
-            except:
+            else:
                 logging.info('ERROR: %s' % github_location)
-                pass
+            #github_location = code.url.split('github.com/')[1]
+            #github_user, github_repo = github_location.split('/')
+            #github_api_url = 'https://api.github.com/repos/%s/%s?client_id=%s&client_secret=%s' % (
+            #    github_user.lower(), github_repo.lower(),
+            #    CLIENT_ID, CLIENT_SECRET
+            #)
+            #
+            #r = requests.get(github_api_url)
+            #data = r.json
+            #
+            #try:
+            #    # handle GitHub API's ISO8601 timestamps
+            #    last_push = data['pushed_at'].strip('Z')
+            #    last_push = dateutil.parser.parse(last_push, fuzzy=True)
+            #    code.repo_last_push = last_push
+            #    # the rest of the API data
+            #    code.repo_forks = data['forks']
+            #    code.repo_watchers = data['watchers']
+            #    code.repo_description = data['description']
+            #    code.repo_master_branch = data['master_branch']
+            #    code.save()
+            #    logging.info('Succesful update: %s' % github_location)
+            #except:
+            #    logging.info('ERROR: %s' % github_location)
+            #    pass
 
         logging.info('Finished update: %s' % datetime.now())
         
