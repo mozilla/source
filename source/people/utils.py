b068fd6b4acba54bd9e4834cc3c41f9ae310bd2c
Ryan Pitts <ryan.a.pitts@gmail.com>
2014-02-20 15:35:00 -0800

    do not create new user record on first login if matching org not found

--- source/people/utils.py
+++ source/people/utils.py
@@ -3,12 +3,14 @@ from django.contrib.auth.models import User, Group
 from .models import Organization, Person
 
 def create_auth_user(email):
-    new_user = User.objects.create_user(email, email)
-
+    new_user = None
     # seek matching organization for new user
     try:
         matching_organization = Organization.objects.get(email__iexact=email)
         organization_admin_group, created = Group.objects.get_or_create(name='Organization Admins')
+
+        # find matching organization before creating user record so we don't have orphans
+        new_user = User.objects.create_user(email, email)
         new_user.groups.add(organization_admin_group)
     except:
         pass
